d9b4f131ccf199761aa60442ba6c0fe08862a442
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-12-16 09:55:21 +0100

    Do not error if there's no reading

--- src/steps/get-site-id.js
+++ src/steps/get-site-id.js
@@ -2,6 +2,9 @@ import * as config from "../common/config";
 import {collection} from "../common/mongodb";
 
 export default function getSiteId (reading) {
+    if (!reading) {
+        return null;
+    }
     return collection(config.SITES_COLLECTION_NAME)
         .findOne({
             sensorsIds: {
