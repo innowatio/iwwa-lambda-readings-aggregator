233df954056682f73df4484771c68508bf20dd0b
Emanuele Busi <emanuele.busi@mondora.com>
2016-06-08 16:59:18 +0200

    Added `AGGREGATES_COLLECTION_NAME` as environment variable

--- src/common/config.js
+++ src/common/config.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.load();
 
 export const MONGODB_URL = process.env.MONGODB_URL;
-export const AGGREGATES_COLLECTION_NAME = "readings-daily-aggregates";
+export const AGGREGATES_COLLECTION_NAME = process.env.AGGREGATES_COLLECTION_NAME || "readings-daily-aggregates";
 export const SITES_COLLECTION_NAME = "sites";
 export const DEBUG = process.env.DEBUG;
